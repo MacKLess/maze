9c9e8a3efe2110cac34933aede533b4f8a532590
Kelsey Langlois and LDMacKrell <ldmackrell@>
2017-09-12 16:39:28 -0700

    Add initialize and look methods to room class.

--- lib/room.rb
+++ lib/room.rb
@@ -1,4 +1,18 @@
 #!/usr/bin/env ruby
 
 class Room
+  attr_reader(:coordinates, :sides)
+  def initialize(coordinates, sides)
+    @coordinates = coordinates
+    @sides = {"north" => sides[0], "east" => sides[1], "south" => sides[2], "west" => sides[3]}
+  end
+
+  def look(direction)
+    @sides[direction]
+  end
+
+  def exit(direction)
+  end
+
+
 end
--- spec/room_spec.rb
+++ spec/room_spec.rb
@@ -4,4 +4,16 @@ require 'rspec'
 require 'room'
 
 describe('Room') do
+  it "stores which sides have doors and which have walls" do
+    room = Room.new([0,0], ["wall", "door", "wall", "wall"])
+    expect(room.sides()).to(eq({"north" => "wall", "east" => "door", "south" => "wall", "west" => "wall"}))
+  end
+  it "stores room's location" do
+    room = Room.new([0,0], ["wall", "door", "wall", "wall"])
+    expect(room.coordinates()).to(eq([0,0]))
+  end
+  it "allows user to look in a direction" do
+    room = Room.new([0,0], ["wall", "door", "wall", "wall"])
+    expect(room.look("north")).to(eq("wall"))
+  end
 end
